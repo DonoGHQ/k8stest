26449b2d0b7978d4567415ee71658ba72f3b6a23
Gautham Parameswaran <91448233+gparameswaran-uptycs@users.noreply.github.com>
2023-11-06 18:52:57 +0800

    Update main.tf
    Signed-off-by: Gautham Parameswaran <91448233+gparameswaran-uptycs@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,66 +1,11 @@
-resource "kubernetes_deployment" "example" {
+resource "kubernetes_cluster_role" "example" {
   metadata {
     name = "terraform-example"
-    namespace = "test-ns"
-    labels = {
-      test = "MyExampleApp"
-    }
   }
 
-  spec {
-    template {
-      spec {
-        host_pid = "false"
-        host_network = "false"
-        host_ipc ="false"
-        container {
-          image = "nginx:1.2"
-          name  = "example"
-          image_pull_policy = "Always"
-          security_context {
-              privileged ="false"
-              allow_privilege_escalation = false
-              read_only_root_filesystem = true
-              capabilities {
-                      drop = ["ALL"]
-                  }  
-          }
-          liveness_probe {
-            http_get {
-              path = "/"
-              port = 80
-
-              http_header {
-                name  = "X-Custom-Header"
-                value = "Awesome"
-              }
-            }
-        }
-          readiness_probe {
-            http_get {
-              path = "/"
-              port = 8080
-                  }
-              }
-        resources {
-            limits = {
-              cpu    = "0.5"
-              memory = "512Mi"
-            }
-            requests = {
-              cpu    = "250m"
-              memory = "50Mi"
-            }
-          }
-
-      }
-          volume {
-              name = "docker-socket"
-              host_path {
-                  path = "/var/run/docker.sock"
-                }
-          }
-    }
+  rule {
+    api_groups = [""]
+    resources  = ["namespaces", "pods"]
+    verbs      = ["get", "list", "watch"]
   }
-}
 }

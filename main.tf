25ff00a9589cb5d19f83bafcd3b2ca99d86c6609
Gautham Parameswaran <91448233+gparameswaran-uptycs@users.noreply.github.com>
2023-11-06 19:19:49 +0800

    Update main.tf
    Signed-off-by: Gautham Parameswaran <91448233+gparameswaran-uptycs@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,25 +1,44 @@
-resource "kubernetes_cluster_role_binding" "example" {
+resource "kubernetes_deployment" "example" {
   metadata {
     name = "terraform-example"
+    namespace = "test-ns"
+    labels = {
+      test = "MyExampleApp"
+    }
   }
-  role_ref {
-    api_group = "rbac.authorization.k8s.io"
-    kind      = "ClusterRole"
-    name      = "cluster-admin"
+  spec {
+    template {
+      spec {
+        container {
+          image = "nginx:sha@12312323235345346345sdff"
+          name  = "example"
+          image_pull_policy = "Always"
+          security_context {
+              read_only_root_filesystem = true
+              capabilities {
+		                  add = ["CAP_SYS_ADMIN"]
+                      drop = ["ALL"]
+                  }  
+          	}
+          readiness_probe {
+            http_get {
+              path = "/"
+              port = 8080
+                  }
+              }
+        resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+
+      }
+    }
   }
-  subject {
-    kind      = "User"
-    name      = "admin"
-    api_group = "rbac.authorization.k8s.io"
-  }
-  subject {
-    kind      = "ServiceAccount"
-    name      = "sa"
-    namespace = "kube-system"
-  }
-  subject {
-    kind      = "Group"
-    name      = "system:masters"
-    api_group = "rbac.authorization.k8s.io"
-  }
+}
 }
